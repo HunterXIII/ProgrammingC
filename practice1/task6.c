63827a315df865b00dd3c5b56ad6f8401954550f
HunterXIII <volkraftroman@gmail.com>
2025-02-13 15:15:55 +0400

    Solved practice1/task6

--- practice1/task6.c
+++ practice1/task6.c
@@ -2,7 +2,15 @@
 
 int main() {
     int count;
-    scanf("%i\n", &count);
-    int two = 0, tree = 0, four = 0;
-    // TODO
+    scanf("%i", &count);
+    
+    for (int four = 0; four <= count / 4; four++) {
+        for (int three = 0; three <= count - four * 4; three++) {
+            for (int two = 0; two <= count - four * 4 - three * 3; two++) {
+                if (count - four * 4 - three * 3 - two * 2 == 0) {
+                    printf("4-seater: %d, 3-seater: %d, 2-seater: %d\n", four, three, two);
+                }
+            }
+        }
+    }
 }
